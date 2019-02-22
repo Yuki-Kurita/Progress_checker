9c6603aaeeedc5e963a2377f8cb9676769830693
Yuki-Kurita <kurita.yuki.kq7@is.naist.jp>
2019-02-22 15:17:47 +0900

    push

--- replyMessage.php
+++ replyMessage.php
@@ -97,6 +97,7 @@ class replyLineMessage{
       $name = $task['name'];
       array_push($json_task, ['type'=>'action','action'=>['type'=>'message','label'=>"$name",'text'=>"$name"]]);
     }
+    array_push($json_task,['type'=>'action','action'=>['type'=>'message','label'=>'確認','text'=>'確認']],['type'=>'action','action'=>['type'=>'message','label'=>'やめる','text'=>'やめる']])
     $client->replyMessage([
     'replyToken'=> $event['replyToken'],
     'messages'=> [
